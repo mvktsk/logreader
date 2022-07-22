9ce0e12459e01233fbb712e1104ca2f2753be0f1
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2022-07-22 21:05:57 +0700

    feat: fix regexp
    Added date format substructed seconds

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import * as fs from 'fs';
 
 const filePath: string = process.argv[2];
-const regExp = /(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/;
+const event = 'NOK';
+const regExp =
+  /\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) (?:[01]\d|2[0123]):(?:[012345]\d)/;
+
+let nokCountArray = new Map<string, number>();
 
 if (!filePath) {
   throw new Error('Please provide a file path as an argument');
 }
 
 try {
-  const buffer: string = fs.readFileSync(filePath, 'utf8');
-  buffer.split(/\r?\n/).forEach(line => {
+  const fileContent: string = fs.readFileSync(filePath, 'utf8');
+  fileContent.split(/\r?\n/).forEach(line => {
     const match = regExp.exec(line);
-    if (match) {
+    if (line.indexOf(event) > -1 && match) {
       console.log(`${match[0]} - ${line}`);
     }
   });
